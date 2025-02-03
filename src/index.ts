ac96d884eee55a8762b5402a6aabe92eaf719c2c
Aleksandr Marchenko <77555146+PlatenPrime@users.noreply.github.com>
2025-02-03 07:26:52 +0200

    inited the server with typescript

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+const app = express();
+
+// Middleware
+
+app.use(cors());
+app.use(express.json());
+
+
+
+// Constants
+const PORT = process.env.PORT || 3232;
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_NAME = process.env.DB_NAME;
+
+
+
+async function start() {
+  try {
+    await mongoose.connect(
+      `mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.b6qtdz4.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`
+    );
+    app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+start();
