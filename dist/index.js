9f2bad86ae86f06345e1efd019fcaa5f0216da60
Aleksandr Marchenko <77555146+PlatenPrime@users.noreply.github.com>
2025-04-13 11:02:06 +0300

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -5,7 +5,7 @@ import mongoose from "mongoose";
 import artRoute from "./modules/arts/router.js";
 import authRoute from "./modules/auth/router.js";
 import { clerkMiddleware } from "@clerk/express";
-import "./types/express/index.d.js";
+// import "./types/express/index.d.js";
 dotenv.config();
 const app = express();
 // Middleware
