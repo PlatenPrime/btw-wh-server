48e9131d2c17fcd7dc7effa5bf6418d8b3f26e89
Aleksandr Marchenko <77555146+PlatenPrime@users.noreply.github.com>
2025-11-05 06:45:46 +0200

    new btw bot

--- src/constants/telegram.ts
+++ src/constants/telegram.ts
@@ -1,6 +1,6 @@
-const btw1 = "6777916786:";
-const btw2 = "AAG1HB5d9spnsql";
-const btw3 = "YM3zIV8C5SFa4JA7GV-E";
+const btw1 = "8161980779:";
+const btw2 = "AAEc-wMjQy7H2g_";
+const btw3 = "7whhK4ws13bhCrrVvlzA";
 export const BTW_TOKEN = btw1 + btw2 + btw3;
 export const BTW_CHAT_ID = "-1002121224059";
 export const BTW_DEFS_CHAT_ID = "-1003183753234";
